38c316565cbd73c87d39e04e5d23899b4225c95a
HoussSak <99645587+HoussSak@users.noreply.github.com>
2023-02-20 13:31:00 +0100

    adding connexion and inscription buttons and their animations

---
+++ src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className=''>
+        <div className='footer-container'>
+            <p>Projet réalisé par Houssem Sakher 2023</p>
+            <p>Les icones Wolverine, Iron-man, Spider-man, bat-man sont prises sur iconFinder</p>
+        </div>
+
+    </footer>
+  )
+}
+
+export default Footer
