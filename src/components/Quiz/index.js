e2850865bd86118881d153f32b1af1a620830a32
HoussSak <99645587+HoussSak@users.noreply.github.com>
2023-02-23 10:20:54 +0100

    add button and options style

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -13,7 +13,9 @@ export class Quiz extends Component {
     storedQuestions:[],
     question:null,
     options:[],
-    idQuestion:0
+    idQuestion:0,
+    btnDisabled:true,
+    userAnswer:null
 
   }
 
@@ -42,14 +44,24 @@ export class Quiz extends Component {
       question:this.state.storedQuestions[this.state.idQuestion].question,
       options:this.state.storedQuestions[this.state.idQuestion].options
     })
-   }
-    
+   }  
+  }
+
+  submitAnswer= (selectedAnswer)=> {
+    this.setState({
+      userAnswer:selectedAnswer,
+      btnDisabled:false
+    })
   }
   
   render() {
 
     const displayOptions =  this.state.options.map((option,index)=> {
-      return <p key={index} className='answerOptions'>{option}</p>
+      return <p key={index}
+               className= {`answerOptions ${this.state.userAnswer === option ? 'selected' : ''}`}
+               onClick={()=> this.submitAnswer(option)}
+               >{option}
+             </p>
     })
 
     //const {pseudo} = this.props.userData;
@@ -60,7 +72,7 @@ export class Quiz extends Component {
         <ProgressBar/>
         <h2>{this.state.question}</h2>
         {displayOptions}
-        <button className='btnSubmit'>Suivant</button>
+        <button disabled={this.state.btnDisabled} className='btnSubmit'>Suivant</button>
       </div>
     )
   }
